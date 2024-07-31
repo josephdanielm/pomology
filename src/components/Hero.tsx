e941b2762f8fb57382063bd9688812f5b182ded6
Joseph Mudd <josephmuddjr@gmail.com>
2024-07-30 19:12:22 -0500

    chore(hero): adjust wording | add temporary cards

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,28 @@
 import { Link } from 'react-router-dom'
-import ssdImage from '../assets/ssd-image.jpg'
+import ProductCard from './ProductCard'
 
 export default function Hero() {
   return (
-    <section className='grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12'>
-      <div className='mr-auto place-self-center lg:col-span-7'>
-        <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl leading-10'>
-          Cutting-Edge Electronics for Every Need
+    <section className='flex flex-col max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 '>
+      <div className='mx-auto place-self-center w-full'>
+        <h1 className='max-w-3xl mx-auto mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl leading-10 text-center'>
+          Exquisite Fruits for Every Occasion
         </h1>
-        <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl'>
-          From the latest gadgets to essential accessories, our products simplify your tech life. Discover why customers
-          around the world trust TechStack for all their electronic needs.
+        <p className='max-w-prose mx-auto text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl'>
+          From the juiciest berries to exotic tropical finds, our selection celebrates the art of fruit cultivation.
+          Experience the essence of nature with Pomology.
         </p>
         <Link
           to='/shop'
-          className='inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100'
+          className='flex w-fit mx-auto items-center justify-center mb-8 lg:mb-10 px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100'
         >
           Shop Now
         </Link>
       </div>
-      <div className='hidden lg:mt-0 lg:col-span-5 lg:flex pl-12'>
-        <img src={ssdImage} alt='Closeup image of an SSD' />
+      <div className='max-w-screen-lg w-full mx-auto px-4 grid grid-cols-3 gap-3'>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   )
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import coconut from '../assets/fruits/coconut.png'
 export default function ProductCard() {
   return (
     <div className='relative group '>
-      <div className=' flex flex-col text-gray-700 bg-clip-border w-full rounded-xl bg-gray-200 p-6 shadow-[inset_-12px_-8px_40px_#46464620]'>
+      <div className=' flex flex-col text-gray-700 bg-clip-border w-full rounded-xl bg-gray-100 p-6 shadow-[inset_-12px_-8px_40px_#46464620]'>
         <img src={coconut} className='max-w-16 self-center py-6' />
         <div className='flex flex-col mx-auto items-center gap-y-4'>
           <h2 className='text-xl font-medium'>Coconut</h2>
@@ -20,7 +20,7 @@ export default function ProductCard() {
             width='40px'
           >
             <path
-              vector-effect='non-scaling-stroke'
+              vectorEffect='non-scaling-stroke'
               d='M226.67-80q-27 0-46.84-19.83Q160-119.67 160-146.67v-506.66q0-27 19.83-46.84Q199.67-720 226.67-720h100v-6.67q0-64 44.66-108.66Q416-880 480-880t108.67 44.67q44.66 44.66 44.66 108.66v6.67h100q27 0 46.84 19.83Q800-680.33 800-653.33v506.66q0 27-19.83 46.84Q760.33-80 733.33-80H226.67Zm0-66.67h506.66v-506.66h-100v86.66q0 14.17-9.61 23.75-9.62 9.59-23.84 9.59-14.21 0-23.71-9.59-9.5-9.58-9.5-23.75v-86.66H393.33v86.66q0 14.17-9.61 23.75-9.62 9.59-23.84 9.59-14.21 0-23.71-9.59-9.5-9.58-9.5-23.75v-86.66h-100v506.66ZM393.33-720h173.34v-6.67q0-36.33-25.17-61.5-25.17-25.16-61.5-25.16t-61.5 25.16q-25.17 25.17-25.17 61.5v6.67ZM226.67-146.67v-506.66 506.66Z'
             />
           </svg>
